111a50a6db04cc6cbc8a7b74b09673ddb35c3cf2
lwh <321@qq.com>
2024-04-20 20:08:12 +0800

    代码生成器后端 初始

---
+++ src/main/java/com/wh/web/constant/FileConstant.java
@@ -0,0 +1,15 @@
+package com.wh.web.constant;
+
+/**
+ * 文件常量
+ *
+ * @author wh
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://generator-1318302941.cos.ap-nanjing.myqcloud.com";
+}
---
+++ src/main/java/com/wh/web/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wh.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wh.web.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author wh
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
