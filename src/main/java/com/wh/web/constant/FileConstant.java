9be01ea549f8948697a07dccba5207ad4a166556
lwh <321@qq.com>
2024-05-21 22:59:57 +0800

    新增使用生成器接口

---
+++ src/main/java/com/wh/web/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.wh.web.constant;
+
+/**
+ * 文件常量
+ *
+ * @author wh
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     */
+    String COS_HOST = "https://generator-1318302941.cos.ap-nanjing.myqcloud.com";
+}
---
+++ src/main/java/com/wh/web/model/dto/generator/GeneratorUseRequest.java
@@ -0,0 +1,23 @@
+package com.wh.web.model.dto.generator;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Data
+public class GeneratorUseRequest implements Serializable {
+
+    /**
+     *  生成器id
+     */
+    private Long id;
+
+    /**
+     * 数据模型
+     */
+    private Map<String,Object> dataModel;
+
+
+    private static final long serialVersionUID = 1L;
+}
